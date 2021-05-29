ff3bf8bf1e9472c0e28b5c9a5eb1670e14544649
Bruno Eufrasio <bruno10tere@gmail.com>
2021-05-29 08:52:21 -0300

    first commit :)

---
+++ src/main/java/com/process_automation/process_automation/ProcessAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.process_automation.process_automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessAutomationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessAutomationApplication.class, args);
+    }
+
+}
